288ee7fffc61d6d782a0ec1bdcf0d6fa0c106fbc
Jackson Makl <127900729+jacksonlmakl@users.noreply.github.com>
2024-09-10 11:46:01 -0400

    Update Client.py

--- CORE/Client.py
+++ CORE/Client.py
@@ -1,6 +1,7 @@
 import os
 from CORE.userDataFrame import userDataFrame
 from CORE.postgres import sql
+import pandas as pd 
 
 class Client:
     def __init__(self,email,policies_json, users_json, secrets_json):
@@ -11,4 +12,11 @@ class Client:
     def table(self,database_name,schema_name,table_name, policies_json, users_json, secrets_json):
         df= sql(f"""select * from "{database_name}"."{schema_name}"."{table_name}" """, secrets_json)
         return userDataFrame(self.email,df,policies_json, users_json, secrets_json)
+    def query(self,query, policies_json, users_json, secrets_json):
+        df = sql(f"""{query}""", secrets_json)
+
+        if type(df) == pd.DataFrame:
+            return userDataFrame(self.email,df,policies_json, users_json, secrets_json)
+        else:
+            return pd.DataFrame([{'Query Executed': query}])
     
